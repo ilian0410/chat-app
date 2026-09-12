e65f4f3c18161b38a931e8423f1da468e2e41c39
ilian0410 <pm_afgoun@esi.dz>
2026-09-12 20:52:27 +0100

    final app

--- lib/view/friend_requests_view.dart
+++ lib/view/friend_requests_view.dart
@@ -3,7 +3,6 @@ import 'package:chat_app/models/friend_request_model.dart';
 import 'package:chat_app/theme/app_theme.dart';
 import 'package:chat_app/view/widgets/friend_request_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
